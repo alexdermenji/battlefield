472a24300432758d31d555733443cc156446cf82
alexdermenji <allexdermenji@gmail.com>
2021-02-05 18:18:35 +0000

    model and view objects implemented

--- js/script.js
+++ js/script.js
@@ -1,36 +1,68 @@
-var randomLoc = Math.floor(Math.random() * 5);
-var location1 = randomLoc;
-var location2 = randomLoc + 1;
-var location3 = randomLoc + 2;
+const view = {
+  displayMessage: function (msg) {
+    const messageArea = document.querySelector(".messageArea");
+    messageArea.innerHTML = msg;
+  },
+  displayHit: function (location) {
+    const cell = document.getElementById(location);
+    cell.setAttribute("class", "hit");
+  },
+  displayMiss: function (location) {
+    const cell = document.getElementById(location);
+    cell.setAttribute("class", "miss");
+  },
+};
 
-var guess;
-var hits = 0;
-var guesses = 0;
-var isSunk = false;
-
-while (isSunk == false) {
-    guess = prompt('Ready, aim, fire! Enter a number 0-6:');
-
-    if (guess < 0 || guess > 6) {
-        alert('Please enter valid number');
-    }
-    else {
-        guesses = guesses + 1;
-        if (guess == location1 ||
-            guess == location2 ||
-            guess == location3) {
-            hits = hits + 1;
-            alert('HIT');
-            if (hits == 3) {
-                isSunk = true;
-                alert('You sank my battleship');
-            }
+const model = {
+  boardSize: 7,
+  numShips: 3,
+  shipLength: 3,
+  shipSunk: 0,
+  ships: [
+    {
+      locations: ["06", "16", "26"],
+      hits: ["", "", ""],
+    },
+    {
+      locations: ["24", "34", "44"],
+      hits: ["", "", ""],
+    },
+    {
+      locations: ["10", "11", "12"],
+      hits: ["", "", ""],
+    },
+  ],
+  fire: function (guess) {
+    for (let i = 0; i < this.numShips; i++) {
+      let ship = this.ships[i];
+      let index = ship.locations.indexOf(guess);
+      if (index >= 0) {
+        ship.hits[index] = "hit";
+        view.displayHit(guess);
+        view.displayMessage("Hit!");
+        if (this.isSunk(ship)) {
+          view.displayMessage("You sank my battleship!");
+          this.shipsSunk++;
         }
-        else {
-            alert('MISS')
-        }
+        return true;
+      }
     }
-}
-var stats = 'You took ' + guesses + ' guesses to sink the battleship, '
-    + 'which means your shooting accuracy was ' + (3 / guesses);
-alert(stats);
+    view.displayMiss(guess);
+    view.displayMessage("You missed!");
+    return false;
+  },
+  isSunk: function (ship) {
+    for (let i = 0; i < this.shipLength; i++) {
+      if (ship.hits[i] !== "hit") {
+        return false;
+      }
+    }
+    return true;
+  },
+};
+
+let controller = {
+  guesses: 0,
+
+  processGuess: function (guess) {},
+};
